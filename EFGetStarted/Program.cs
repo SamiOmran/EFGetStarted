92b64e234b7c09fc675bcb5b674c6880594c3870
Sami Imran <samiimran2016@gmail.com>
2021-01-10 19:32:26 +0200

    first step

---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
